71ad79a078e49705f15d413c933ee4a2cd606ceb
Shaina Veloz <shainaveloz@Shainas-MacBook-Air.local>
2016-05-18 19:22:57 -0400

    added user creation function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,27 @@
 var doggieData = new Firebase("https://doggie-daycare.firebaseio.com/");
 
 
-//Getting a snapshot of the database before
+// user creation function
+doggieData.createUser({
+  email: "bobtony@gmail.com",
+  password: "correcthorsebatterystaple"
+}, function(error, userData) {
+  if (error) {
+    switch (error.code) {
+      case "EMAIL_TAKEN":
+        console.log("The new user account cannot be created because the email is already in use.");
+        break;
+      case "INVALID_EMAIL":
+        console.log("The specified email is not a valid email.");
+        break;
+      default:
+        console.log("Error creating user:", error);
+    }
+  } else {
+    console.log("Successfully created user account with uid:", userData.uid);
+  }
+});
+
 doggieData.on("value", function(snapshot) {
 
 
