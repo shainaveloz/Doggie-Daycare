3425a2b4805ea7d0c8979d142d2ecd21c435f581
shainaveloz <shaina.veloz@gmail.com>
2016-05-28 02:40:45 -0400

    added to user login functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,147 +15,56 @@ var storageRef = storage.ref();
 
 $('#loginButton').on('click', function(){
 
-  var username = $('#username');
-  var pw = $('#password');
-  if (username == names && password == pwds) {
+  var names = $('#username');
+  var pwds = $('#password');
+  var email = $('#email');
+  if (username == names && password == pwds && email == email) {
       storageRef.set({
-        names: username,
-        pwds: password
+        names: username
+      })
+      storageRef.child('names').set({
+        pwds: password,
+        email:email
       })
     // loads to backend user page
     window.location.replace("http://index.html");
 
-  } else if(username !== names && password !== pwds) {
+  } else if(username !== names && password !== pwds && email !== email) {
     storageRef.push({
-        names: username,
-        pwds: password
+        names: username
+    })
+    storageRef.child('names').push({
+        pwds: password,
+        email: email
     })
   }
   
 });
 
+//listener for 'enter' in username input
+$('#username').keypress(function(e) {
+  if (e.keyCode == 13 && $('#username').val() !== "") {
+    username = capitalize($('#username').val());
+    password = capitalize($('#password').val());
+    email = capitalize($('#email').val());
+    window.location.replace("http://index.html");
+  }
+});
 
-// storageRef.on('value', function(snapshot){
-
-//   // Getting a snapshot of the database before
-//   firebase.auth().onAuthStateChanged(function(user) {
-//    if (user) {
-//       // User is signed in.
-//    } else {
-//       // No user is signed in.
-//    }
-
-//     var user = firebase.auth().currentUser;
-//   })
-
-// })
-
-//Getting a snapshot of the database before
-//firebase.auth().onAuthStateChanged(function(user) {
-//  if (user) {
-    // User is signed in.
-//  } else {
-    // No user is signed in.
-//  }
-//})
-//
-//var user = firebase.auth().currentUser;
-//console.log(user, "this is user var line 27")
-//if (user) {
-  // User is signed in.
-//} else {
-  // No user is signed in.
-//}
-//
-//var name, email, photoUrl, uid;
-//
-//if (user != null) {
-//  name = user.displayName;
-//  email = user.email;
-//  photoUrl = user.photoURL;
-//  uid = user.uid;
-//}
-//
-//
-//if (user != null) {
-//  user.providerData.forEach(function (profile) {
-//    console.log("Sign-in provider: "+profile.providerId);
-//    console.log("  Provider-specific UID: "+profile.uid);
-//    console.log("  Name: "+profile.displayName);
-//    console.log("  Email: "+profile.email);
-//    console.log("  Photo URL: "+profile.photoURL);
-//  });
-//}
-//user.updateProfile({
-//  displayName: "Jane Q. User",
-//  photoURL: "https://example.com/jane-q-user/profile.jpg"
-//}).then(function() {
-  // Update successful.
-//}, function(error) {
-  // An error happened.
-//});
-//
-//
-//user.updateEmail("user@example.com").then(function() {
-  // Update successful.
-//}, function(error) {
-  // An error happened.
-//});
-//
-//var newPassword = getASecureRandomPassword();
-//
-//user.updatePassword(newPassword).then(function() {
-  // Update successful.
-//}, function(error) {
-  // An error happened.
-//});
-//
-//var auth = firebase.auth();
-//var emailAddress = "user@example.com";
-//
-//auth.sendPasswordResetEmail(emailAddress).then(function() {
-  // Email sent.
-//}, function(error) {
-  // An error happened.
-//});
-//
-//
-//user.delete().then(function() {
-  // User deleted.
-//}, function(error) {
-  // An error happened.
-//});
-//
-//var credential;
-
-// Prompt the user to re-provide their sign-in credentials
-
-//user.reauthenticate(credential).then(function() {
-//  // User re-authenticated.
-//}, function(error) {
-//  // An error happened.
-//});
+//Function to capitalize usernames
+function capitalize(name) {
+  return names.charAt(0).toUpperCase() + names.slice(1);
+}
 
 
+storageRef.on('value', function(snapshot){
+  var names = $('#username').val().trim();
+  var email = $('#email').val().trim();
+  var pwds = $('#password').val().trim;
+ 
+})
 
-//
-//storageRef.set({
-//    user : user
-//    });
 
-
-//
-//var updateProfile = storageRef.child('displayName').put(name);
-//var updateEmail = storageRef.child('email').put(updateEmail);
-//var updatePassword = storageRef.child('password').put(updatePassword);
-//var sendPasswordResetEmail = storageRef.child('resetEmail').put(sendPasswordResetEmail);
-
-//
-//push({ (this is done if the user is not already there)
-//    displayName: name,
-//    Email: email
-//
-//})
 
 
 
